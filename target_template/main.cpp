1f04f7adac4c8d9959230d644f02fdd2e169ff7e
Davide Rizzi <d.rizzi@novalabs.io>
2018-01-02 12:39:38 +0100

    Updated copyright

--- target_template/main.cpp
+++ target_template/main.cpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016-2018 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+ 
 /* USB Module template file
  *
  */

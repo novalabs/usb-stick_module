12ef72049c9b0f2e8e12bdd84646ecf44f6e8e12
Davide Rizzi <d.rizzi@novalabs.io>
2018-06-18 17:09:37 +0200

    Removed bootmaster code (deprecated choice)

--- src/Module.cpp
+++ src/Module.cpp
@@ -87,51 +87,6 @@ RTCANConfig rtcan_config = {
     1000000, 100, 60
 };
 
-#if CORE_IS_BOOTLOADER_BRIDGE
-core::mw::bootloader::MessageType _bootMasterMessageType = core::mw::bootloader::MessageType::MASTER_ADVERTISE;
-
-void
-Module::setBootloaderMasterType(
-    core::mw::bootloader::MessageType type
-)
-{
-    _bootMasterMessageType = type;
-}
-
-void
-bootloader_master_node(
-    void* arg
-)
-{
-    core::mw::Node node("bootmaster");
-    core::mw::Publisher<core::mw::bootloader::BootMasterMsg> pub;
-    core::mw::bootloader::BootMasterMsg* msgp;
-
-    node.advertise(pub, BOOTLOADER_MASTER_TOPIC_NAME, core::os::Time::INFINITE);
-
-    (void)arg;
-    chRegSetThreadName("bootmaster");
-
-    while (true) {
-        if (pub.alloc(msgp)) {
-            msgp->command    = _bootMasterMessageType;
-            msgp->sequenceId = 0;
-
-            msgp->data[0] = 0;
-            msgp->data[1] = 0;
-            msgp->data[2] = 0;
-            msgp->data[3] = 0;
-            msgp->data[4] = 0;
-            msgp->data[5] = 0;
-
-            pub.publish_remotely(*msgp);
-        }
-
-        core::os::Thread::sleep(core::os::Time::ms(500));
-    }
-} // bootloader_master_node
-#endif // if CORE_IS_BOOTLOADER_BRIDGE
-
 
 Module::Module()
 {}
@@ -177,10 +132,6 @@ Module::initialize()
         rtcantra.initialize(rtcan_config, canID());
 
         core::mw::Middleware::instance().start();
-
-#if CORE_IS_BOOTLOADER_BRIDGE
-        core::os::Thread::create_heap(NULL, 1024, core::os::Thread::PriorityEnum::NORMAL, bootloader_master_node, nullptr);
-#endif
 
         initialized = true;
     }
